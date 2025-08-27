ee42b993a499cde0c529668d61041b8be10e6c19
maxiangxi <maxiangxi815@outlook.com>
2025-08-27 11:22:23 +0800

    修正网关服务Redis配置

---
+++ microservices/gateway-service/src/main/java/org/demo/gateway/config/RedisConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Redis配置类
+ * 配置Redis连接和RedisTemplate
+ * 
+ * @author Baoleme Team
+ * @version 1.0
+ * @since 2025-01-25
+ */
+package org.demo.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Redis配置类
+ * 配置订单服务的Redis连接和操作模板
+ */
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 配置RedisTemplate Bean
+     * 
+     * @param connectionFactory Lettuce连接工厂
+     * @return RedisTemplate<String, Object> Redis操作模板
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
